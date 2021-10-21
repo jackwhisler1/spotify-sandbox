d11d7459431bdfe68c746f7621b870dff04e880d
jack <whisler1@gmail.com>
2021-10-20 20:17:58 -0500

    adds top_songs action, incomplete functionality

--- app/controllers/api/spotify_controller.rb
+++ app/controllers/api/spotify_controller.rb
@@ -15,9 +15,32 @@ class Api::SpotifyController < ApplicationController
       client_secret: Rails.application.credentials.spotify_api_key[:client_secret]
     })
 
-    access_token = response.parse["access_token"]
-    response = HTTP.auth("Bearer #{access_token}").get("https://api.spotify.com/v1/me")
+    @access_token = response.parse["access_token"]
+    response = HTTP.auth("Bearer #{@access_token}").get("https://api.spotify.com/v1/me")
     render json: response.parse
+  end
+
+  def top_songs()
+    response = HTTP.auth("Bearer BQBQZXdIZ7l-58FvthLAOTxpZ1EMnv8pA4n2MQtVey54y9N767nb60540-orfjwsybM9EOSzndanDJgEHkG51IR_exWMvyBdK842XnJDX6ENZZ2oPxnkZc4foKzewwWOVoVZJNVw-bFaj_vOFKZrKw").get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=200&offset=1")
+    response = response.parse
+    songs = []
+    response["items"].each do |song|
+      songs << {
+       "title": song["name"],
+       "artist": song["artists"][0]["name"]
+      }
+    end
+    render json: songs
+  end
+
+  def song_ids()
+    response = HTTP.auth("Bearer BQCBV_dJT9PS28jSVq0Woppd7ypYfuIx4Fdqw527AIBlakS9AgDTmM9EGZWm2ePbgB-XKjbokwpvkHjOiTxYdwaq75J8B-FL79svNMHaYL7RLfyFavy8hhhQpBnhyVkcqJ27zbItNo85dUbXzztepw").get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=200&offset=1")
+    response = response.parse
+    ids = ""
+    response["items"].each do |song|
+      ids += song["id"] + ","
+    end
+    render json: ids
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     get "/spotify_authorize" => "spotify#spotify_authorize"
     get "/spotify/callback" => "spotify#spotify_callback"
-    
-
-    get "/index" => "tracks#index"
-    get "/songs/100" => "tracks#top100"
+    get "/spotify/top_songs" => "spotify#top_songs"
+    get "/spotify/song_ids" => "spotify#song_ids"
   end
+  
+  get "/index" => "tracks#index"
+  
 end
